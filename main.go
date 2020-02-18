f821031c589a4f234e75921c94826d430384f125
zondaTW <u226699@gmail.com>
2020-02-18 12:08:50 +0800

    Add Date type for json and sql

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"database/sql/driver"
 	"fmt"
 	"net/http"
 	"os"
@@ -13,13 +14,55 @@ import (
 )
 
 type Todo struct {
-	ID            int       `json:"id" form:"id"`
-	Title         string    `json:"title" form:"title"`
-	Stared        bool      `json:"stared" form:"stared"`
-	DeadlineStart time.Time `json:"deadline_start" form:"deadline_start"`
-	DeadlineEnd   time.Time `json:"deadline_end" form:"deadline_end"`
-	Comment       string    `json:"comment" form:"comment"`
-	Completed     bool      `json:"completed" form:"completed"`
+	ID            int    `json:"id" form:"id"`
+	Title         string `json:"title" form:"title"`
+	Stared        bool   `json:"stared" form:"stared"`
+	DeadlineStart Date   `json:"deadline_start" form:"deadline_start"`
+	DeadlineEnd   Date   `json:"deadline_end" form:"deadline_end"`
+	Comment       string `json:"comment" form:"comment"`
+	Completed     bool   `json:"completed" form:"completed"`
+}
+
+type Date time.Time
+
+func (date Date) Value() (driver.Value, error) {
+	return []byte(time.Time(date).Format(`"2006-01-02"`)), nil
+}
+
+func (date *Date) Scan(v interface{}) error {
+	switch s := v.(type) {
+	case time.Time:
+		*date = Date(s)
+	case []byte:
+		newTime, err := time.Parse("2006-01-02", string(s))
+		if err != nil {
+			return err
+		}
+		*date = Date(newTime)
+	case string:
+		newTime, err := time.Parse("2006-01-02", s)
+		if err != nil {
+			return err
+		}
+		*date = Date(newTime)
+	default:
+		return fmt.Errorf("date: Unsupport scanning type %T", v)
+	}
+	return nil
+}
+
+func (date *Date) UnmarshalJSON(input []byte) error {
+	newTime, err := time.Parse(`"2006-01-02"`, string(input))
+	if err != nil {
+		return err
+	}
+
+	*date = Date(newTime)
+	return nil
+}
+
+func (date Date) MarshalJSON() ([]byte, error) {
+	return []byte(time.Time(date).Format(`"2006-01-02"`)), nil
 }
 
 type Todos []Todo
