f260d9f60df58ecbb2f86c4646852b8b1f57659b
zondaTW <u226699@gmail.com>
2020-02-20 12:50:57 +0800

    Add middleware package

--- route.go
+++ route.go
@@ -1,9 +1,12 @@
 package main
 
-import "github.com/zondaTW/go-todolist-server/todos"
+import (
+	"github.com/zondaTW/go-todolist-server/middleware"
+	"github.com/zondaTW/go-todolist-server/todos"
+)
 
 func initRoute() {
-	router.Use(CORSMiddleware)
+	router.Use(middleware.CORSMiddleware)
 	api := router.Group("/api")
 	todos.Route(api, db)
 }
