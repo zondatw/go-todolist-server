b66e0eb31601f1842c79b09c5ad31b13638e81ef
zonda yang <u226699@gmail.com>
2021-01-23 21:48:29 +0800

    Add test: TestGetTodo

--- todos/model_test.go
+++ todos/model_test.go
@@ -40,3 +40,33 @@ func TestQueryTodoTable(t *testing.T) {
 	newTodos := queryTodoTable(db)
 	assert.Equal(t, newTodos, todos, "they should be equal")
 }
+
+func TestGetTodo(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	deadlineStart, _ := time.Parse("2006-01-02", "2021-01-20")
+	deadlineEnd, _ := time.Parse("2006-01-02", "2021-01-21")
+
+	todosMockRows := sqlmock.NewRows([]string{"id", "title", "stared", "deadline_start", "deadline_end", "comment", "completed"}).
+		AddRow(1, "Test title", false, deadlineStart, deadlineEnd, "Test comment", false)
+
+	mock.ExpectQuery("^SELECT (.+) FROM todos*").
+		WillReturnRows(todosMockRows)
+
+	var todo Todo = Todo{
+		ID:            1,
+		Title:         "Test title",
+		Stared:        false,
+		DeadlineStart: lib.Date(deadlineStart),
+		DeadlineEnd:   lib.Date(deadlineEnd),
+		Comment:       "Test comment",
+		Completed:     false,
+	}
+
+	newTodo := getTodo(db, "1")
+	assert.Equal(t, newTodo, todo, "they should be equal")
+}
